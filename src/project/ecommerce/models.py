12a4b9723e51e864d18bd7be81cf1e9e70f7f05e
Nisarg Shah <nisshah1499@gmail.com>
2020-06-25 19:52:19 +0530

    changed naming conventions

--- src/project/ecommerce/models.py
+++ src/project/ecommerce/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.conf import settings
 
 class Product(models.Model):
-	productId = models.AutoField(primary_key=True)
-	productName = models.CharField(max_length=100)
-	productCategory = models.TextField()
-	productPrice = models.FloatField()
-	productDiscountPrice = models.FloatField(blank=True, null=True)
-	productPreviewDesc = models.CharField(max_length=200, null=True)
-	productFullDesc = models.CharField(max_length=400, null=True)
+	product_id = models.AutoField(primary_key=True)
+	product_name = models.CharField(max_length=100)
+	product_category = models.TextField()
+	product_price = models.FloatField()
+	product_discount_price = models.FloatField(blank=True, null=True)
+	product_preview_desc = models.CharField(max_length=200, null=True)
+	product_full_desc = models.CharField(max_length=400, null=True)
 
 class OrderProduct(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	orderItem = models.ForeignKey(Product, on_delete=models.CASCADE)
-	orderQuantity = models.IntegerField(default = 1)
+	order_item = models.ForeignKey(Product, on_delete=models.CASCADE)
+	order_quantity = models.IntegerField(default = 1)
 
--- src/project/ecommerce/schema.py
+++ src/project/ecommerce/schema.py
@@ -10,33 +10,33 @@ class ProductType(DjangoObjectType):
 		model = Product
 
 class Query(graphene.ObjectType):
-	products = graphene.List(ProductType, productName = graphene.String())
+	products = graphene.List(ProductType, product_name = graphene.String())
 
-	def resolve_products(self, info, productName, **kwargs):
-		filter = (Q(productName__icontains = productName))
+	def resolve_products(self, info, product_name, **kwargs):
+		filter = (Q(product_name__icontains = product_name))
 		return Product.objects.filter(filter)
 
 class AddProduct(graphene.Mutation):
 	addProduct = graphene.Field(ProductType)
 
 	class Arguments:
-		productName = graphene.String(required=True)
-		productCategory = graphene.String(required=True)
-		productPrice = graphene.Float(required=True)
-		productDiscountPrice = graphene.Float()
-		productPreviewDesc = graphene.String()
-		productFullDesc = graphene.String(required=True)
+		product_name = graphene.String(required=True)
+		product_category = graphene.String(required=True)
+		product_price = graphene.Float(required=True)
+		product_discount_price = graphene.Float()
+		product_preview_desc = graphene.String()
+		product_full_desc = graphene.String(required=True)
 
-	def mutate(self, info, productName, productCategory, productPrice, productDiscountPrice, productPreviewDesc, productFullDesc, **kwargs):
-		productDiscountPrice = kwargs.get('productDiscountPrice', None)
-		productPreviewDesc = kwargs.get('productPreviewDesc', None)
+	def mutate(self, info, product_name, product_category, product_price, product_discount_price, product_preview_desc, product_full_desc, **kwargs):
+		product_discount_price = kwargs.get('product_discount_price', None)
+		product_preview_desc = kwargs.get('product_preview_desc', None)
 
-		product = Product(productName=productName,
-					productCategory=productCategory,
-					productPrice=productPrice,
-					productDiscountPrice=productDiscountPrice,
-					productPreviewDesc=productPreviewDesc,
-					productFullDesc=productFullDesc)
+		product = Product(product_name=product_name,
+					product_category=product_category,
+					product_price=product_price,
+					product_discount_price=product_discount_price,
+					product_preview_desc=product_preview_desc,
+					product_full_desc=product_full_desc)
 
 		product.save()
 
